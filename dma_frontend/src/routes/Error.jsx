b01f33d09e1ef9277c7a7776feda0c3c333e0a79
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2022-07-29 12:01:19 +0100

    remove unwanted

--- dma_frontend/src/routes/Error.jsx
+++ dma_frontend/src/routes/Error.jsx
@@ -1,6 +1,6 @@
 import { Button, Typography } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
