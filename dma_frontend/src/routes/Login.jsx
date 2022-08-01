926d0581d30c19ef1432806cd3d5e60677ab25ef
Uche <uchaynwankwo@gmail.com>
2022-08-01 11:46:32 +0100

    arrange login page

--- dma_frontend/src/routes/Login.jsx
+++ dma_frontend/src/routes/Login.jsx
@@ -1,6 +1,8 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/login.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const LoginPage = () => {
   return (
@@ -32,6 +34,36 @@ const LoginPage = () => {
         </nav>
       </header>
       <Typography>Login Page</Typography>
+      <div className="info">
+        <div className="image">
+          <img src="" alt="logo" />
+        </div>
+        <h2>Welcome back!</h2>
+        <p>Please enter your details</p>
+        <div className="login-form">
+          <form action="" method="post">
+              <label for="email">Email</label>
+              <input type="email" name="email" id="email" placeholder="johndoe@email.com" />
+              <label for="password">Password</label>
+              <ion-icon name="lock-closed-outline" id="lock-closed-outline"></ion-icon>
+              <input type="password" name="password" id="password" />
+              <ion-icon name="eye-off-outline" id="eye-off-outline"></ion-icon>
+              <div className="login-remember"> 
+                    <label>
+                        {/* <input type="checkbox" name="login-remember" id="login-remember" checked /> */}
+                        <ion-icon name="checkmark-circle-outline"></ion-icon>
+                        {/* <ion-icon name="ellipse-outline"></ion-icon> */}
+                        Remember me
+                    </label>            
+              </div>
+              <input type="button" value="Sign In" />             
+              <div>
+                  <p>Forgot password? <a href="/">Reset Password</a> </p>
+                  <p>Don't have an account? <a href="/">Sign Up</a></p>
+              </div>
+          </form>
+        </div>
+      </div> 
     </div>
   );
 };
