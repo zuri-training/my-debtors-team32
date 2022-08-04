29bb99cb7c2c24a56b14600ccd926ed0ebecb445
Ibukun <ibk2k7@gmail.com>
2022-08-04 23:12:07 +0100

    added about us page component

---
+++ dma_frontend/src/components/AboutUsComp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/About-us-page.css';
+
+const AboutUsComp = () => {
+  return (
+    <div className=''>
+      <div className=''>
+        {/* TYPE YOUR CODE HERE. IF YOU USE TAILWINDCSS MAKE SURE TO ADD THE tw- PREFIX TO THE UTILITY CLASS AND IF YOU WRITE VANILLA CSS KINDLY USE contact-us.css FILE */}
+      </div>
+    </div>
+  );
+};
+
+export default AboutUsComp;
--- dma_frontend/src/routes/About.jsx
+++ dma_frontend/src/routes/About.jsx
@@ -1,47 +1,12 @@
 import React from 'react';
+import AboutUsComp from '../components/AboutUsComp';
 import ProjectLayoutComp from '../components/layout/ProjectLayout';
 import '../styles/About-us-page.css';
 const AboutPage = () => {
   return (
     <div>
       <ProjectLayoutComp>
-        <div className='head1'>
-          <div className='what'>
-            <h2>What we do</h2>
-          </div>
-
-          <div className='header-2'></div>
-        </div>
-
-        <div className='purpose'>
-          <h2>Our purpose</h2>
-          <br />
-          <p>
-            Debitor Monitoring Agency is a platform used by school management to
-            verify the authencity of parents and financial prudence. we help you
-            build reliable financial systems y fishing out potential debtors
-          </p>
-          <br />
-          <p>
-            Our aim is to help more schools become financially independent. we
-            are concerned about the way schools go bankrupt due to extended
-            debts, so this platform is being set up to help sschool managements
-            trace their debtors and recover their losses
-          </p>
-        </div>
-        <div className='body-1'>
-          <div className='child-1'>
-            <h2>Our Focus</h2> <br />
-            <br />
-            <p>
-              Our focus is to help you attain financial freedom by closing out
-              every financial gap possible
-            </p>
-            <br />
-            <button className=' btn'>Learn More &rsaquo; </button>
-          </div>
-          <div className='child-2'></div>
-        </div>
+        <AboutUsComp />
       </ProjectLayoutComp>
     </div>
   );
