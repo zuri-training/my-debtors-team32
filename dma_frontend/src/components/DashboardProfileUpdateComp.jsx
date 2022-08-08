3d5c62d899243ad68cf7b2e94b7c38bb31b40e53
System Administrator <root@macs-MacBook-Pro.local>
2022-08-08 23:47:02 +0100

    school-list page

--- dma_frontend/src/components/DashboardProfileUpdateComp.jsx
+++ dma_frontend/src/components/DashboardProfileUpdateComp.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/dashboard-profile.css';
 
 function DashboardProfileUpdateComp() {
-  return (
+  return(
     <div className=''>
-      <div className=''>Profile Update</div>
+      <div className=''>Profile Upade</div>
     </div>
   );
+
 }
 
 export default DashboardProfileUpdateComp;
