3524c3880397e6ea5589cdd9f528a584cf502677
Freshiah Catherine Njenga <87461018+CatherineNjenga@users.noreply.github.com>
2022-08-09 21:14:25 +0300

    Adding Forgot PAssword page

--- dma_frontend/src/components/ForgotPasswordComp.jsx
+++ dma_frontend/src/components/ForgotPasswordComp.jsx
@@ -6,7 +6,38 @@ const ForgotPasswordComp = () => {
   return (
     <div>
         <HeaderLayoutComp />
-        {/* Type your code here */}
+        {
+          <div class="form-container">
+          <div class="image">
+              <img src="./forgotPass.jpg" alt="forgot-password"/>
+          </div>
+          <h2>Don't panic, Recover your account with ease!</h2>
+          <div class="forgot-pwd-form">
+              <p>Please enter your details</p>
+              <form action="" method="post" class="first-form">
+                  <label for="email">Email</label>
+                  <input type="email" name="email" id="email" placeholder="Enter Your Email"/>       
+              </form>
+              <p class="code">A code will be sent to your email</p>
+              <button type="submit" class="btn receive-btn">Receive Code</button>
+              <form action="" method="post" class="second-form">
+                  <div>
+                      <label for="code">Enter code</label>
+                      <input type="password" name="code" id="code"/>
+                  </div>
+                  <div>
+                      <label>New Password</label>
+                      <input type="new-password" name="new-password" id="new-password"/>
+                  </div>
+                  <div>
+                      <label for="confirm-password">Confirm password</label>
+                      <input type="confirm-password" name="confirm-password" id="confirm-password"/>
+                  </div>
+                  <button type="submit" class="btn submit-btn">Submit</button>    
+              </form>    
+          </div>    
+    </div>    
+        }
     </div>
   )
 }
