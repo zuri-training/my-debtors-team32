9cbaab9668a3462d03c6c8ac1bb868992615a607
Uche <uchaynwankwo@gmail.com>
2022-08-11 10:24:47 +0100

    add classname to forgot password

--- dma_frontend/src/components/UpdateDebtorComp.jsx
+++ dma_frontend/src/components/UpdateDebtorComp.jsx
@@ -7,35 +7,35 @@ const UpdateDebtorComp = () => {
     <div>
         <HeaderLayoutComp />
         {
-          <div class="debtor-form">   
+          <div className="debtor-form">   
           <h1>Update Debtor</h1>
             <form action="" method="post">
-              <div class="grid-container"> 
-                <label class="block">
+              <div className="grid-container"> 
+                <label className="block">
                   Student Name
                   <input type="text" placeholder="Enter Student's Name"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Name of parent
-                  <input type="text" placeholder="Enter Parent's name" class="block"/>
+                  <input type="text" placeholder="Enter Parent's name" className="block"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Email
                   <input type="email" name="email" id="email" placeholder="Enter Parent's Email"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Phone number
                   <input type="tel" name="tel" id="tel" placeholder="Enter Parent's Phone Number"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Amount
                   <input type="number" name="amount" id="amount" placeholder="Enter Amount"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Debt status
                   <input type="text" name="item" id="item" placeholder="Enter Debtor's Name"/>
                 </label>
-                <label class="block">
+                <label className="block">
                   Name of School
                   <input type="text" name="item" id="item" placeholder="Enter School's Name"/>
                 </label>
