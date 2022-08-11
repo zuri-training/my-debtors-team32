221b8bf1777250de78a5450e3ceb638d2adcb3d8
Uche <uchaynwankwo@gmail.com>
2022-08-11 10:17:49 +0100

    Add class names to forgot password

--- dma_frontend/src/components/ForgotPasswordComp.jsx
+++ dma_frontend/src/components/ForgotPasswordComp.jsx
@@ -7,20 +7,20 @@ const ForgotPasswordComp = () => {
     <div>
         <HeaderLayoutComp />
         {
-          <div class="form-container">
-          <div class="image">
+          <div className="form-container">
+          <div className="image">
               <img src="./forgotPass.jpg" alt="forgot-password"/>
           </div>
           <h2>Don't panic, Recover your account with ease!</h2>
-          <div class="forgot-pwd-form">
+          <div className="forgot-pwd-form">
               <p>Please enter your details</p>
-              <form action="" method="post" class="first-form">
+              <form action="" method="post" className="first-form">
                   <label for="email">Email</label>
                   <input type="email" name="email" id="email" placeholder="Enter Your Email"/>       
               </form>
-              <p class="code">A code will be sent to your email</p>
-              <button type="submit" class="btn receive-btn">Receive Code</button>
-              <form action="" method="post" class="second-form">
+              <p className="code">A code will be sent to your email</p>
+              <button type="submit" className="btn receive-btn">Receive Code</button>
+              <form action="" method="post" className="second-form">
                   <div>
                       <label for="code">Enter code</label>
                       <input type="password" name="code" id="code"/>
@@ -33,7 +33,7 @@ const ForgotPasswordComp = () => {
                       <label for="confirm-password">Confirm password</label>
                       <input type="confirm-password" name="confirm-password" id="confirm-password"/>
                   </div>
-                  <button type="submit" class="btn submit-btn">Submit</button>    
+                  <button type="submit" className="btn submit-btn">Submit</button>    
               </form>    
           </div>    
     </div>    
